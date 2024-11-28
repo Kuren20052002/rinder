231efed4fa9262389d91005f24382b5d82c29c94
Nguyễn Hoàng Lâm <165152517+Kuren20052002@users.noreply.github.com>
2024-11-28 10:53:50 +0700

    Add search photos function

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,10 @@ class PhotosController < ApplicationController
     if params[:random_img]
       @random_img = Unsplash::Photo.random
     end
+
+    if params[:img_search_word]
+      @random_img = nil
+      @search_imgs = Unsplash::Photo.search(params[:img_search_word])
+    end
   end
 end
